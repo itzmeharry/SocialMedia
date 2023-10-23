0699677a27659643466758ef3bc539fdc1ef6df5
Hariharan E <139750998+itzmeharry@users.noreply.github.com>
2023-10-23 06:49:08 -0700

    Update Home.js

--- my-project/src/Home.js
+++ my-project/src/Home.js
@@ -5,14 +5,6 @@ import DataContext from './Context/DataContext'
 const Home = () => {
   const {searchResults, fetchError, isLoding } = useContext(DataContext);
   return (
-//     <main className='Home'>
-//  {posts.length ? (<Feed posts={posts} />) : (<p style={{marginTop: "2rem"}}>
-//   No posts to dispaly.
-//  </p>)}
-//     </main>
-
-//useAxiosFetch use panni ipdi pannanum 
-//useAxiosFetch nana create panna oru hook state 
 <main className='Home'>
   {isLoding && <p className='statusMsg'>Loding Posts......</p>}
   {!isLoding && fetchError && <p className='statusMsg' style={{color:"red"}}>{fetchError}</p>}
